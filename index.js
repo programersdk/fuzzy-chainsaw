f1a29c475713028446a0d976f41740c3f87261ba
Muhammad Rehan <rehanbhaifromsdk@gmail.com>
2025-08-24 10:04:40 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var num = Math.floor(Math.random() * 100) + 1;
-
+var chans = 5;
+ document.getElementById("chnes").innerText = "your attampt ::"+" " +(chans);
 function chek() {
+       if(chans==0){
+        var over = document.getElementById("hide")
+          over.style.display ="none"
+         document.getElementById("result").innerText = "Game over......!";  
+       }else{
     var data = document.getElementById("inputdata").value;
     data = Number(data);
 
@@ -8,5 +14,7 @@ function chek() {
         document.getElementById("result").innerText = "🎉 Well done! Correct guess.";
     } else {
         document.getElementById("result").innerText = "❌ Try again...";
-    }
+        chans--;
+    }}
+     document.getElementById("chnes").innerText = "your attampt"+" " +(chans+1);
 }
